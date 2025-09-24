08d59bd5e1d35ae39fa1d9bd30aef1c4498737ef
Fernando Castillo Lozano <fernando25798@gmail.com>
2025-09-24 20:01:33 +0200

    primer commit JS

---
+++ 01-js-mio/Basic-Mio/01-variables.js
@@ -0,0 +1,11 @@
+//var
+var helloWorld = "Hello World"
+console.log(helloWorld)
+
+//let --> la buena forma de definir variables
+let helloWorld2 = "Hello World 2"
+console.log(helloWorld2)
+
+//const --> la buena forma de definir variables
+const helloWorld3 = "Hello World 3"
+console.log(helloWorld3)
