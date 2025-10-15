8a8b615db062c79482428370aeb413bb65a0a72c
Fernando Castillo Lozano <fernando25798@gmail.com>
2025-10-15 15:53:33 +0200

    JS intermediate

---
+++ 01-js-mio/Intermediate-Mio/00-advanced-functions.js
@@ -0,0 +1,20 @@
+// Funciones avanzadas
+// ciudadanos de primera clase
+
+const greet = function (name) {
+    console.log(`Hola, ${name}`)
+}
+
+greet("Brais")
+
+function processGreeting(greetFunction, name) {
+    greetFunction(name)
+}
+
+function returnGreeting() {
+    return greet
+}
+
+processGreeting(greet, "MoureDev")
+const greet2 = returnGreeting()
+greet2("Brais Moure")
